52415840dd87c70c820521704b67f00875430f2a
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-14 15:08:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p ./${APP_FOLDER}/build/outputs/apk/upload
 cp ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
-zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping.zip -x ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
+zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
 cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 
 hub checkout ${${REPO_BRANCH}:-master}
