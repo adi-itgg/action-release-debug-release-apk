1c12a7b87c28f14227f915c32d032340994cf300
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-14 15:46:18 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs
 zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
 cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 
-hub checkout ${${REPO_BRANCH}:-master}
+hub checkout ${REPO_BRANCH}
 VERSION_NAME=$(cat ./"${APP_FOLDER}"/version.txt)
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/upload/*.* -m "${RELEASE_TITLE} - v${VERSION_NAME}" v"${VERSION_NAME}"
