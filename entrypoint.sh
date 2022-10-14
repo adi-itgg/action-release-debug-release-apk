f269a2de61b6067376308bc84702163862928fb7
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-14 13:34:15 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir ./${APP_FOLDER}/build/outputs/apk/upload
+mkdir -p ./${APP_FOLDER}/build/outputs/apk/upload
 cp ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk ./${APP_FOLDER}/build/outputs/apk/upload/
-cp /${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
+cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
+cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
