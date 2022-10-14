def7f64b9e721b92e71d81753b3ac9637f09c401
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-14 13:57:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs
 cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 
 hub checkout ${${REPO_BRANCH}:-master}
-VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
+VERSION_NAME=$(cat ./"${APP_FOLDER}"/version.txt)
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/upload/*.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" v"${VERSION_NAME}"
