51971b80c43240d1e803e16ee5fce74e7fb20c2f
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-16 08:17:52 +0700

    Include aab build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 mkdir -p ./${APP_FOLDER}/build/outputs/apk/upload
 cp ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
+cp ./${APP_FOLDER}/build/outputs/bundle/debug/*.aab ./${APP_FOLDER}/build/outputs/apk/upload/
+cp ./${APP_FOLDER}/build/outputs/bundle/release/*.aab ./${APP_FOLDER}/build/outputs/apk/upload/
 zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
 cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 
 hub checkout ${REPO_BRANCH}
-hub fetch --tags
 VERSION_NAME=$(cat ./"${APP_FOLDER}"/version.txt)
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/upload/*.apk -a ./${APP_FOLDER}/build/outputs/apk/upload/*.zip -m "${RELEASE_TITLE} - v${VERSION_NAME}" "v${VERSION_NAME}"
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/upload/*.apk -a ./${APP_FOLDER}/build/outputs/apk/upload/*.aab -a ./${APP_FOLDER}/build/outputs/apk/upload/*.zip -m "${RELEASE_TITLE} - v${VERSION_NAME}" "v${VERSION_NAME}"
