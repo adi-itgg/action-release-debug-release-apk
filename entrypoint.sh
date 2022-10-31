32f27e4c142332d6b531b87bb9b80d6b32da0fde
Syahdilla Putra Sholeh <54009445+adi-itgg@users.noreply.github.com>
2022-10-31 09:10:48 +0700

    Build apk and aab with mappings file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 mkdir -p ./${APP_FOLDER}/build/outputs/apk/upload
+
+./gradlew assembleRelease --stacktrace
+
+cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 cp ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk ./${APP_FOLDER}/build/outputs/apk/upload/
 cp ./${APP_FOLDER}/build/outputs/apk/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
+zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping_apk.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
+
+./gradlew bundleRelease --stacktrace
+
 cp ./${APP_FOLDER}/build/outputs/bundle/debug/*.aab ./${APP_FOLDER}/build/outputs/apk/upload/
 cp ./${APP_FOLDER}/build/outputs/bundle/release/*.aab ./${APP_FOLDER}/build/outputs/apk/upload/
-zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
-cp ./${APP_FOLDER}/release/*.apk ./${APP_FOLDER}/build/outputs/apk/upload/
+zip -9 -r ./${APP_FOLDER}/build/outputs/apk/upload/mapping_aab.zip ./${APP_FOLDER}/build/outputs/mapping/release/mapping.txt
 
 hub checkout ${REPO_BRANCH}
 VERSION_NAME=$(cat ./"${APP_FOLDER}"/version.txt)
